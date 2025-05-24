9ff0115b44306f6e3de4709716f5083674f4e83c
Twig Nielsen <littleb88888888@gmail.com>
2025-05-24 14:26:05 -0600

    Added labels to camera view grid

--- scripts/cameras/camera_view.gd
+++ scripts/cameras/camera_view.gd
@@ -3,6 +3,7 @@ extends Control
 
 @export var viewport_container: SubViewportContainer
 @export var viewport: SubViewport
+@export var label: Label
 @export var viewport_size: int = 100
 
 var camera: TrackingCamera
@@ -13,11 +14,12 @@ func _ready() -> void:
         viewport_container.size.x / viewport_size
     )).call_deferred()
 
-func initialize_camera(camera_position: Vector3):
+func initialize_camera(camera_position: Vector3, title: String):
     camera = TrackingCamera.new()
     viewport.add_child(camera)
     camera.position = camera_position
     camera.make_current()
+    label.text = title
 
 func set_camera(source_camera: TrackingCamera):
     camera.copy_from(source_camera)
--- scripts/cameras/camera_view_grid.gd
+++ scripts/cameras/camera_view_grid.gd
@@ -10,5 +10,6 @@ func _ready() -> void:
         var view_instance: CameraView = camera_view_prefab.instantiate()
         add_child(view_instance)
         view_instance.initialize_camera(
-            camera_positions_parent.get_child(i).global_position
+            camera_positions_parent.get_child(i).global_position,
+            "Cam %d" % (i + 1)
         )
