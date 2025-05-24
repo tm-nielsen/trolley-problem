a2444d2c5af88fc8ff2295a7857a23b32397397c
Twig Nielsen <littleb88888888@gmail.com>
2025-05-24 10:35:52 -0600

    Tuned movement - adjusted cart friction implementation and parameters - rotated cart velocity with cart - adjusted movement forces

--- scripts/player/cart_body.gd
+++ scripts/player/cart_body.gd
@@ -4,8 +4,8 @@ extends CharacterBody3D
 @export var push_force: float = 4
 @export var turn_force: float = 4
 @export var jump_force: float = 2
-@export_range(0, 1) var friction: float = 0.1
-@export_range(0, 1) var angular_friction: float = 0.1
+@export var friction: float = 1
+@export var angular_friction: float = 10
 
 @export_subgroup("paddles")
 @export var right_paddle: PaddleController
@@ -23,9 +23,10 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
     velocity += get_gravity() * delta
     if is_on_floor():
-        velocity *= get_friction_multiplier(friction, delta)
-        angular_velocity *= get_friction_multiplier(angular_friction, delta)
+        velocity *= max(1 - friction * delta, 0)
+        angular_velocity *= max(1 - angular_friction * delta, 0)
     rotate_y(angular_velocity)
+    velocity = velocity.rotated(Vector3.UP, angular_velocity)
     move_and_slide()
 
 
@@ -36,8 +37,3 @@ func apply_push(push_scale: float, turn_direction: float):
 func apply_jump(opposite_paddle: PaddleController):
     if is_on_floor(): velocity.y += jump_force
     elif opposite_paddle.was_jump_flicked(): velocity.y += jump_force * 4
-
-
-
-func get_friction_multiplier(f: float, delta: float) -> float:
-    return max(1 - f * delta * Engine.physics_ticks_per_second, 0);
