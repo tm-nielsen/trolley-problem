ce28d54050ba27e4fa771989d6b808721768d737
Twig Nielsen <littleb88888888@gmail.com>
2025-05-25 10:30:39 -0600

    Adjusted game resolution - changed project window resolution - adjusted theme spacing - removed viewport scaling from camera views

--- scripts/cameras/camera_view.gd
+++ scripts/cameras/camera_view.gd
@@ -1,18 +1,11 @@
 class_name CameraView
 extends Control
 
-@export var viewport_container: SubViewportContainer
 @export var viewport: SubViewport
 @export var label: Label
-@export var viewport_size: int = 100
 
 var camera: TrackingCamera
 
-
-func _ready() -> void:
-    (func(): viewport_container.stretch_shrink = int(
-        viewport_container.size.x / viewport_size
-    )).call_deferred()
 
 func initialize(camera_position: Vector3, title: String):
     camera = TrackingCamera.new()
