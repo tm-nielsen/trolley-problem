369ed5dca4ae96c970c712b86fabc0386ec8ab0a
Twig Nielsen <littleb88888888@gmail.com>
2025-05-25 13:59:20 -0600

    Added more game timer juice

--- scripts/ui/font_scaled_label.gd
+++ scripts/ui/font_scaled_label.gd
@@ -3,9 +3,9 @@ extends Label
 
 @onready var base_font_size = label_settings.font_size
 
-func scale_for(font_scale: float, duration: float):
+func scale_for(font_scale: float, duration: float) -> Tween:
     _set_font_scale(font_scale)
-    TweenHelpers.call_delayed(_set_font_scale, duration)
+    return TweenHelpers.call_delayed(_set_font_scale, duration)
 
 func _set_font_scale(font_scale: float = 1):
     label_settings.font_size = int(base_font_size * font_scale)
--- scripts/ui/game_timer.gd
+++ scripts/ui/game_timer.gd
@@ -7,20 +7,31 @@ extends FontScaledLabel
 @export var second_scale: float = 0.8
 @export var ten_second_scale: float = 0.6
 @export var second_scale_duration: float = 0.1
-@export var ten_second_scale_duration: float = 0.4
+@export var ten_second_scale_duration: float = 0.2
+@export var collect_scale: float = 1.5
+@export var collect_scale_duration: float = 0.2
 
 var time: float
+var tick_tweens_enabled: bool = true
 
 
 func _ready() -> void:
     time = starting_time
     GlobalSignalBus.item_collected.connect(
-        func(_item): time += collection_bonus
+        func(_item):
+            time += collection_bonus
+            tick_tweens_enabled = false
+            scale_for(collect_scale,
+                collect_scale_duration
+            ).tween_callback(
+                func(): tick_tweens_enabled = true
+            )
     )
 
 func _process(delta: float) -> void:
     time -= delta
-    if floor(time + delta) != floor(time):
+    var is_second: bool = floor(time + delta) != floor(time)
+    if tick_tweens_enabled && is_second:
         if int(time) % 10 == 0:
             scale_for(ten_second_scale,
                 ten_second_scale_duration
