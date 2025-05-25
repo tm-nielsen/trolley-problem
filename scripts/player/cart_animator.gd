eb4c8d327bf3aa57d9bbaa53d49345ebe9cfe76f
Twig Nielsen <littleb88888888@gmail.com>
2025-05-24 20:42:59 -0600

    Implemented cart wiggle

--- scripts/player/cart_animator.gd
+++ scripts/player/cart_animator.gd
@@ -12,13 +12,65 @@ extends Node3D
 @export var material_hidden: ShaderMaterial
 @export var material_collided: ShaderMaterial
 
-var current_body_mesh_index: int
+@export_subgroup("wiggle", "wiggle")
+@export var wiggle_target: BoneAttachment3D
+@export var wiggle_rotation_sensitivity := Vector2(50, 50)
+@export var wiggle_position_sensitivity := Vector3(1, 1, 1)
+@export_subgroup("wiggle/resources", "elastic_wiggle")
+@export var elastic_wiggle_rotation: ElasticVector2
+@export var elastic_wiggle_position: ElasticVector2
+@export var elastic_wiggle_height: ElasticValue
+
 var collision_tween: Tween
+var current_body_mesh_index: int
+
+var default_wiggle_transform: Transform3D
+var previous_position: Vector3
+var previous_velocity: Vector3
 
 
 func _ready() -> void:
     current_body_mesh_index = surface_index_collided
     show_mesh_pose(surface_index_normal)
+    default_wiggle_transform = wiggle_target.transform
+
+
+func _process(delta: float) -> void:
+    var aligned_acceleration := update_movement_values()
+    update_elastic_values(aligned_acceleration, delta)
+    apply_elastic_values()
+
+
+func update_elastic_values(acceleration: Vector3, delta: float):
+    var planar_acceleration := Vector2(acceleration.x, acceleration.z)
+    elastic_wiggle_rotation.value_velocity += planar_acceleration
+    elastic_wiggle_rotation.update_value(Vector2.ZERO, delta)
+    elastic_wiggle_position.value_velocity += planar_acceleration
+    elastic_wiggle_position.update_value(Vector2.ZERO, delta)
+    elastic_wiggle_height.value_velocity += acceleration.y
+    elastic_wiggle_height.update_value(0, delta)
+
+
+func apply_elastic_values():
+    wiggle_target.transform = default_wiggle_transform
+    wiggle_target.position += Vector3(
+        elastic_wiggle_position.x,
+        elastic_wiggle_height.value,
+        elastic_wiggle_position.y
+    )
+    wiggle_target.rotation += Vector3(
+        elastic_wiggle_rotation.y,
+        0,
+        elastic_wiggle_rotation.x
+    )
+
+
+func update_movement_values() -> Vector3:
+    var velocity := global_position - previous_position
+    var acceleration := velocity - previous_velocity
+    previous_position = global_position
+    previous_velocity = velocity
+    return acceleration * basis.inverse()
 
 
 func show_mesh_pose(surface_index: int, material := material_body):
