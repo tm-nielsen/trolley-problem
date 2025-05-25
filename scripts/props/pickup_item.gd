a8931ec10026ca4487a39b2e225650dfff9a78c6
Twig Nielsen <littleb88888888@gmail.com>
2025-05-24 23:56:03 -0600

    Implemented pickup item

---
+++ scripts/props/pickup_item.gd
@@ -0,0 +1,33 @@
+extends Area3D
+
+signal collected
+
+@export var default_item: PackedScene
+@export var mesh: MeshInstance3D
+
+
+func _ready():
+    show_item(default_item)
+    body_entered.connect(_on_body_entered)
+
+
+func show_item(prefab: PackedScene):
+    if !prefab: return
+    var instance: Node3D = prefab.instantiate()
+    var prefab_mesh = _find_mesh_instance(instance)
+    mesh.mesh = prefab_mesh.mesh
+    instance.queue_free()
+
+
+func _find_mesh_instance(parent: Node3D) -> MeshInstance3D:
+    for child in parent.get_children():
+        if child is MeshInstance3D: return child
+        var child_search = _find_mesh_instance(child)
+        if child_search != null: return child_search
+    return null
+
+
+func _on_body_entered(body):
+    if body is CartBody:
+        collected.emit()
+        queue_free()
