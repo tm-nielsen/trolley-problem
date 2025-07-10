8de455f0b34def57f0e5186ddd1601fbc757964f
Twig <littleb88888888@gmail.com>
2025-07-09 18:29:49 -0600

    Randomly offset limited fps views

--- scripts/cameras/limited_framerate_camera_view.gd
+++ scripts/cameras/limited_framerate_camera_view.gd
@@ -5,11 +5,17 @@ extends CameraView
 
 
 func initialize(camera_position: Vector3, title: String):
-    super(camera_position, title)
-    var update_tween := create_tween()
-    update_tween.set_loops()
-    update_tween.tween_callback(update_view)
-    update_tween.tween_interval(1.0 / fps)
+	super(camera_position, title)
+
+	var delay_tween = create_tween()
+	delay_tween.tween_interval(fps * randf() / fps)
+	delay_tween.tween_callback(
+		func():
+		var update_tween := create_tween()
+		update_tween.set_loops()
+		update_tween.tween_callback(update_view)
+		update_tween.tween_interval(1.0 / fps)
+	)
 
 func update_view():
-    viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+	viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
