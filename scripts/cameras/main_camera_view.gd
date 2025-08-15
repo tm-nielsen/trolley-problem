8277e9a06016741e32eebc20c1c8d696d318b4e4
Twig Nielsen <littleb88888888@gmail.com>
2025-08-15 13:43:19 -0600

    Prevented camera swapping in tutorial

--- scripts/cameras/main_camera_view.gd
+++ scripts/cameras/main_camera_view.gd
@@ -7,6 +7,7 @@ extends CameraView
 @export var fov_tween_duration: float = 0.3
 
 var fov_tween: Tween
+var enabled: bool = false
 
 
 func _ready():
@@ -14,6 +15,7 @@ func _ready():
     viewport.audio_listener_enable_3d = true
 
 func _process(_delta: float) -> void:
+    if !enabled: return
     for i in CameraViewGrid.VIEW_COUNT:
         if Input.is_action_just_pressed("camera %d" % (i + 1)):
             set_camera(view_grid.cameras[i])
@@ -27,3 +29,6 @@ func start_fov_tween():
     fov_tween.tween_property(
         camera, "fov", fov, fov_tween_duration
     )
+
+func enable():
+    enabled = true
